de92bcaee529012d812394ca479e896d7a5a6bd0
João Netto <hi@joaonetto.me>
2014-03-18 17:41:42 -0300

    Initial dist subtree commit

---
+++ scripts/5429841a.main.js
@@ -0,0 +1,1 @@
+"use strict";var __konami__;$(window).on("load",function(){$(".jumbotron.masthead").css("height",$(this).innerHeight())}),$(document).ready(function(){__konami__.enable(function(){$("#home,#release,#social").addClass("rainbow")},{replay:!1})});
