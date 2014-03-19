fb792de43516920abfeb74175e6ac4927f3f52c7
João Netto <hi@joaonetto.me>
2014-03-19 16:46:35 -0300

    New fucking release

--- scripts/d3c05327.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";var __konami__;$(window).on("load",function(){$(".jumbotron.masthead").css("height",$(this).innerHeight()),$(".full-height").css("height",$(this).innerHeight()+200)}),$(document).ready(function(){__konami__.enable(function(){$("#home,#release,#social").addClass("rainbow")},{replay:!1}),$(".carousel").carousel()});
