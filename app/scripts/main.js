4aa3f91c6366c46af3f19b48be28034cf2125675
João Netto <hi@joaonetto.me>
2014-03-19 16:44:20 -0300

    Contact section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,28 @@
 'use strict';
 var __konami__;
 
+// Fixing height
 $(window).on('load', function () {
 	$('.jumbotron.masthead').css('height', $(this).innerHeight());
-	$('.full-height').css('height', $(this).innerHeight() + 200);
+	$('.full-height').css('height', $(this).innerHeight());
 });
 
 $(document).ready(function () {
+	// Vini Konami
 	__konami__.enable(function(){
-        $('#home,#release,#social').addClass('rainbow');
+        $('#home,#release,#ep,#gallery,#contact').addClass('rainbow');
     }, { replay: false });
 
+	// Gallery
     $('.carousel').carousel();
+
+    // One page Scroll
+    $('.holder').onepage_scroll({
+		sectionContainer: '.section',
+		easing: 'ease',
+		animationTime: 1000,
+		pagination: true,
+		keyboard: true,
+		responsiveFallback: false
+	});
 });
