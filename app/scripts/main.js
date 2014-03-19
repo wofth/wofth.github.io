bcd1c901c8765340ea1583bc233bb655824e517a
João Netto <hi@joaonetto.me>
2014-03-19 18:39:14 -0300

    Adding a few plugins to improve navigation. Refactoring all bootstrap grid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,12 +17,50 @@ $(document).ready(function () {
     $('.carousel').carousel();
 
     // One page Scroll
-    $('.holder').onepage_scroll({
-		sectionContainer: '.section',
-		easing: 'ease',
-		animationTime: 1000,
-		pagination: true,
-		keyboard: true,
-		responsiveFallback: false
+ //    $('.holder').onepage_scroll({
+	// 	sectionContainer: '.section',
+	// 	easing: 'ease',
+	// 	animationTime: 1000,
+	// 	pagination: true,
+	// 	updateURL: true,
+	// 	keyboard: true,
+	// 	responsiveFallback: false
+	// });
+
+	// Youtube videos gallery
+	var youtubeHolder = $('#youtube_gallery');
+
+	$.ajax({
+		type: 'GET',
+		url: 'http://gdata.youtube.com/feeds/users/wofthounds/uploads?alt=json-in-script&format=5',
+		cache: false,
+		dataType: 'jsonp',
+		success: function (data) {
+			var videosLimit = 8, i = 0;
+			$(data.feed.entry).each(function eachVideo (entry) {
+				if(videosLimit == i) return;
+				var url = this.link[0].href,
+					url_thumbnail = this.media$group.media$thumbnail[2].url,
+					description = this.media$group.media$description.$t,
+					title = this.media$group.media$title.$t;
+
+				var html = '<div class="col-xs-3 text-center">';
+					html += '<a href="'+url+'" class="fancybox-media">';
+					html += '<div align="center"><img src="'+url_thumbnail+'" alt="'+description+'" class="img-responsive" /></div>';
+					html += title;
+					html += '</a>';
+					html += '</div>';
+
+				youtubeHolder.append(html);
+				i++;
+			});
+			$('.fancybox-media').fancybox({
+				openEffect  : 'fade',
+				closeEffect : 'fade',
+				helpers : {
+					media : {}
+				}
+			});
+	  	}
 	});
 });
